b0a3cb66d1acc1cdb44e980deda49a55a1f4e1be
bernardohsilva <bernardo.silva2345@gmail.com>
2023-09-26 21:29:58 -0300

    array

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const nomes = [
+    'Ana Maria',
+    'Bruno Alves',
+    'Carlos José',
+    'Daniel Martins',
+    'Efraim Gomes',
+    'Francisco Junior',
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Lista de Nomes</Text>
+      <FlatList
+        data={nomes}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text>{item}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  item: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
 });
+
+export default App;
